1b120b5bcfefba9784e7693deb5c8b92ecfebc62
jeongtunjang <jjy306105@gmail.com>
2025-04-14 22:15:59 +0900

    feat: PlanCardList > public page에 임시로 표시

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+import PlanCardList from "@/components/planCardList/PlanCardList";
 export default function Home() {
   return (
     <div className="main-container">
       <h1>비회원 메인 페이지입니다.</h1>
+      <PlanCardList />
     </div>
   );
 }
