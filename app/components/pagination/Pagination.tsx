b94fa1df2dece729ba94e49c3557fc562cae2ef1
jeongtunjang <jjy306105@gmail.com>
2025-04-14 17:30:39 +0900

    feat: Pagination > 인터페이스 추가 및 UI 반영, 버튼 클릭 이벤트 헨들링

--- app/components/pagination/Pagination.tsx
+++ app/components/pagination/Pagination.tsx
@@ -1,21 +1,84 @@
 "use client";
-import React from "react";
-import Link from "next/link";
-import styles from "./Pagination.module.css";
+
+import { useState } from "react";
+import styles from "./pagination.module.scss";
+import Image from "next/image";
 
 interface PaginationProps {
-  currentPage: number;
-  totalContents: number;
-  paginate: (pageNumber: number) => void;
-  onPageChange: (page: number) => void;
+  totalPages: number;
+  onChangePage?: (page: number) => void;
 }
 
-const Pagination = ({
-  currentPage,
-  totalContents,
-  paginate,
-}): PaginationProps => {
-  return <></>;
+const Pagination = ({ totalPages, onChangePage }: PaginationProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const visiblePages = 5;
+  const groupStartPage =
+    Math.floor((currentPage - 1) / visiblePages) * visiblePages + 1;
+
+  const handleClick = (page: number) => {
+    // 페이지 클릭 시
+    if (onChangePage) {
+      onChangePage(page);
+    }
+    setCurrentPage(page);
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage((prev) => prev - 1);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prev) => prev + 1);
+  };
+
+  // 보여줄 페이지 수
+  const pages = Array.from(
+    { length: visiblePages },
+    (_, i) => groupStartPage + i
+  ).filter((page) => page <= totalPages);
+
+  const isLastPage = totalPages === currentPage;
+  const isFirstPage = currentPage === 1;
+
+  return (
+    <div className={styles.pagination}>
+      <button
+        className={styles.arrow}
+        onClick={handlePrevPage}
+        disabled={isFirstPage}
+      >
+        <Image
+          src={"/icons/chevron-left-primary.svg"}
+          alt="prev"
+          width={20}
+          height={20}
+        />
+      </button>
+
+      {pages.map((page) => (
+        <button
+          key={page}
+          onClick={() => handleClick(page)}
+          className={page === currentPage ? styles.active : ""}
+        >
+          {page}
+        </button>
+      ))}
+
+      <button
+        className={styles.arrow}
+        onClick={handleNextPage}
+        disabled={isLastPage}
+      >
+        <Image
+          src={"/icons/chevron-right-primary.svg"}
+          alt="next"
+          width={20}
+          height={20}
+        />
+      </button>
+    </div>
+  );
 };
 
 export default Pagination;
