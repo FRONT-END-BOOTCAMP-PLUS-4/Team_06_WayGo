4d202579d008d67a55dd4879e52eddf6fc9f1511
okang1014 <okang1014@gmail.com>
2025-04-15 17:14:23 +0900

    Revert "[enhance] 셀렉트 박스 컴포넌트 수정 (#48)"
    This reverts commit 2704fac1cd5290c99604fc0bb5e2368a9460c784.

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,36 +1,4 @@
-import SelectBasic from "@/components/selectBasic/selectBasic";
-
 export default function Home() {
-  const durationOptionList = [
-    { value: 1, title: "당일치기" },
-    { value: 2, title: "1박2일" },
-    { value: 3, title: "2박3일" },
-    { value: 4, title: "3박4일~" },
-  ];
-
-  const seasonOptionList = [
-    { value: 1, title: "봄 🌸" },
-    { value: 2, title: "여름 🤿" },
-    { value: 3, title: "가을 🍁" },
-    { value: 4, title: "겨울 ❄️" },
-  ];
-
-  const locationOptionList = [
-    { value: 1, title: "수도권" },
-    { value: 2, title: "강원권" },
-    { value: 3, title: "충청권" },
-    { value: 4, title: "호남권" },
-    { value: 5, title: "경상권" },
-    { value: 6, title: "제주권" },
-  ];
-
-  const budgetOptionList = [
-    { value: 1, title: "~10만원" },
-    { value: 2, title: "10~20만원" },
-    { value: 3, title: "20~40만원" },
-    { value: 4, title: "40만원~" },
-  ];
-
   return (
     <div className="main-container">
       <h1>비회원 메인 페이지입니다.</h1>
--- app/components/selectBasic/selectBasic.tsx
+++ app/components/selectBasic/selectBasic.tsx
@@ -3,23 +3,24 @@
 import { useState, useRef, useEffect } from "react";
 import styles from "./selectBasic.module.scss";
 
-interface SelectBasicProps {
-  label?: string;
-  placeholder?: string;
-  option: OptionProps[];
-}
+type OptionProps = {
+  value: string;
+  label: string;
+};
 
-interface OptionProps {
-  value: number;
-  title: string;
-}
-
-const SelectBasic = ({ option, placeholder, label }: SelectBasicProps) => {
-  const [selectedValue, setSelectedValue] = useState<string | number>();
-  const [selectedLabel, setSelectedLabel] = useState(placeholder);
+const SelectBasic = () => {
+  const [selectedValue, setSelectedValue] = useState("");
+  const [selectedLabel, setSelectedLabel] =
+    useState("장소·일정·계절을 검색해보세요");
   const [isOpen, setIsOpen] = useState(false);
   const [isFocused, setIsFocused] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const optionList: OptionProps[] = [
+    { value: "1", label: "옵션1" },
+    { value: "2", label: "옵션2" },
+    { value: "3", label: "옵션3" },
+  ];
 
   const handleToggleDropdown = () => {
     setIsOpen((prev) => {
@@ -35,14 +36,14 @@ const SelectBasic = ({ option, placeholder, label }: SelectBasicProps) => {
   };
 
   const handleSelectOption = (
-    value: number,
-    title: string,
+    value: string,
+    label: string,
     event: React.MouseEvent
   ) => {
     event.stopPropagation();
 
     setSelectedValue(value);
-    setSelectedLabel(title);
+    setSelectedLabel(label);
     closeOption();
   };
 
@@ -64,54 +65,51 @@ const SelectBasic = ({ option, placeholder, label }: SelectBasicProps) => {
   }, []);
 
   return (
-    <>
-      {label && <h1 className={styles.selectLabel}>{label}</h1>}
-      <div
-        ref={dropdownRef}
-        className={`${styles.selectContainer} ${isFocused ? styles.focused : ""}`}
-        onClick={handleToggleDropdown}
-      >
-        <div className={styles.selectWrapper}>
-          <span
-            className={`${styles.placeholder} ${selectedValue ? styles.selected : ""}`}
+    <div
+      ref={dropdownRef}
+      className={`${styles.selectContainer} ${isFocused ? styles.focused : ""}`}
+      onClick={handleToggleDropdown}
+    >
+      <div className={styles.selectWrapper}>
+        <span
+          className={`${styles.placeholder} ${selectedValue ? styles.selected : ""}`}
+        >
+          {selectedLabel}
+        </span>
+        <div className={styles.arrow}>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className={isOpen ? styles.rotated : styles.normal}
           >
-            {selectedLabel}
-          </span>
-          <div className={styles.arrow}>
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className={isOpen ? styles.rotated : styles.normal}
-            >
-              <path
-                d="M7 10L12 15L17 10"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </div>
+            <path
+              d="M7 10L12 15L17 10"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
         </div>
-
-        {isOpen && (
-          <div className={styles.optionsContainer}>
-            {option.map((item: OptionProps) => (
-              <div
-                key={item.value}
-                className={`${styles.optionItem} ${selectedValue === item.value ? styles.selected : ""}`}
-                onClick={(e) => handleSelectOption(item.value, item.title, e)}
-              >
-                {item.title}
-              </div>
-            ))}
-          </div>
-        )}
       </div>
-    </>
+
+      {isOpen && (
+        <div className={styles.optionsContainer}>
+          {optionList.map((option) => (
+            <div
+              key={option.value}
+              className={`${styles.optionItem} ${selectedValue === option.value ? styles.selected : ""}`}
+              onClick={(e) => handleSelectOption(option.value, option.label, e)}
+            >
+              {option.label}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 };
 
