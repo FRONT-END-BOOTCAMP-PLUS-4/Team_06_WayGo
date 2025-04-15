585b677a911c9dfee8048e535387e562f52ef125
okang1014 <okang1014@gmail.com>
2025-04-15 11:19:04 +0900

    [feat] app/public/page.tsx > 테스트용 TextInput 추가
    [feat] app/public/page.tsx > 테스트용 TextInput 추가

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,9 +1,15 @@
 import PwInput from "@/components/pwInput/PwInput";
+import TextInput from "@/components/textInput/TextInput";
 
 export default function Home() {
   return (
     <div className="main-container">
       <h1>비회원 메인 페이지입니다.</h1>
+      <TextInput
+        id="test"
+        placeholder="테스트 인풋입니다."
+        label="텍스트 인풋 테스트"
+      />
       <PwInput />
     </div>
   );
