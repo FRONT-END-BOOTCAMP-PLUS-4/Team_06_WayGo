4aee6ea7d95397310ab041eb83eefd510f5fc2ed
okang1014 <okang1014@gmail.com>
2025-04-15 14:44:26 +0900

    [chore] prettier.config.js > 자동 줄바꿈 설정 추가

--- prettier.config.js
+++ prettier.config.js
@@ -5,4 +5,5 @@ module.exports = {
   trailingComma: "es5", // 객체, 배열 등 끝에 쉼표 추가
   bracketSpacing: true, // 객체 내의 콜론 뒤 공백 추가
   arrowParens: "always", // 함수 매개변수에 괄호 유지
+  endOfLine: "auto", // OS 별 줄바꿈 문자 자동 설정
 };
