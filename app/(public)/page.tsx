fa5d43fc8da3a67663d181632cb777f7d294e305
jeongtunjang <jjy306105@gmail.com>
2025-04-15 16:55:04 +0900

    feat: main page > UI 반영, main style 파일 추가

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+import PlanCardList from "@/components/planCardList/PlanCardList";
+import Button from "@/components/button/Button";
+import SearchInput from "@/components/searchInput/SearchInput";
+import styles from "./main.module.scss";
+
 export default function Home() {
   return (
     <div className="main-container">
-      <h1>비회원 메인 페이지입니다.</h1>
+      <div className={styles["logo-container"]}>
+        <Image
+          src="/logos/logo-slogan.svg"
+          alt="웹사이트 로고"
+          width={240}
+          height={160}
+        />
+        <div className={styles["button-wrapper"]}>
+          <Button size={"medium"} label={"여행 계획 쓰기"} type={"default"} />
+        </div>
+      </div>
+      <div className={styles["search-container"]}>
+        <SearchInput />
+      </div>
+      <PlanCardList />
+      <PlanCardList />
     </div>
   );
 }
