07d22af6d4e27ce3a355293d934986bc357d5fcb
okang1014 <okang1014@gmail.com>
2025-04-16 15:11:19 +0900

    [feat] my-profile > 마이 프로필 페이지 마크업 완료

--- app/(private)/my-profile/page.tsx
+++ app/(private)/my-profile/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import PlanCardList from "@/components/planCardList/PlanCardList";
 import styles from "./myProfile.module.scss";
 import React from "react";
 import UserInfoCard from "@/(private)/my-profile/components/userInfoCard/UserInfoCard";
+import CommentCardList from "@/(private)/my-profile/components/commentedCardList/CommentedCardList";
 
 const MyProfile: React.FC = () => {
   const userInfo = {
@@ -10,11 +13,30 @@ const MyProfile: React.FC = () => {
     profileImg: "/logos/char-success.svg",
   };
 
+  const dummyData = [
+    {
+      id: 1,
+      comment: "정말 멋진 여행이군요",
+      title: "제주 3박 4일 일주 여행",
+      imageUrl: "/images/jeju.jpg",
+    },
+    {
+      id: 2,
+      comment: "저도 저대로 따라갔더니 기가 막히더군요",
+      title: "서울 한바퀴",
+      imageUrl: "/images/jeju.jpg",
+    },
+  ];
+
   return (
     <div className="main-container">
-      <h1 className={styles["my-profile-header"]}>🙋‍♂️ 마이 페이지</h1>
+      <h1 className={styles["my-profile-header"]}>🙋‍♂️ 마이 프로필</h1>
       <UserInfoCard userInfo={userInfo} />
-      <PlanCardList />
+      <PlanCardList isScrollAvailable={true} />
+      <div className={styles["commented-card-section"]}>
+        <h2>내 댓글이 달린 계획</h2>
+        <CommentCardList data={dummyData} />
+      </div>
     </div>
   );
 };
