d7639d852ee4f16bee4b084b3f0581f90fcb11fa
hjKim1597 <hojinkim971597@gmail.com>
2025-04-16 17:20:04 +0900

    [refactor] 로그인/회원가입 페이지 > 컴포넌트 삭제

--- app/(public)/(auth)/login/page.tsx
+++ app/(public)/(auth)/login/page.tsx
@@ -1,11 +1,42 @@
-// 로그인 페이지
-import LoginForm from "./components/LoginForm";
+import Image from "next/image";
 import styles from "./login.module.scss";
+import TextInput from "@/components/textInput/TextInput";
+import PwInput from "@/components/pwInput/PwInput";
+import Button from "@/components/button/Button";
 
 export default function LoginPage() {
   return (
     <div className={styles.loginContainer}>
-      <LoginForm />
+      <div className={styles.logoContainer}>
+        <Image
+          src="/logos/logo-slogan.svg"
+          alt="웨이고 메인 로고"
+          width={250}
+          height={175}
+        />
+      </div>
+
+      <div className={styles.formContainer}>
+        <div className={styles.inputGroup}>
+          <TextInput
+            id="email"
+            type="email"
+            label="이메일"
+            placeholder="이메일을 입력해주세요."
+            labelSize=""
+          />
+          <PwInput
+            id="login-pw-input"
+            label="비밀번호"
+            placeholder="비밀번호를 입력해주세요."
+          />
+        </div>
+
+        <div className={styles.buttonGroup}>
+          <Button label="로그인" size="full" type="default" />
+          <Button label="회원가입" size="full" type="lined" />
+        </div>
+      </div>
     </div>
   );
 }
--- app/(public)/(auth)/signup/page.tsx
+++ app/(public)/(auth)/signup/page.tsx
@@ -1,13 +1,48 @@
 // 회원가입 페이지
-import SignUpForm from "./components/SignUpForm";
+
 import styles from "./signup.module.scss";
+import TextInput from "@/components/textInput/TextInput";
+import CheckInput from "@/components/checkInput/CheckInput";
+import PwInput from "@/components/pwInput/PwInput";
+import Button from "@/components/button/Button";
 
 export default function SignUpPage() {
   return (
     <div className={styles.signUpContainer}>
       <h1 className={styles.signUpTitle}>회원가입</h1>
-      <div className={styles.signUpFormContainer}>
-        <SignUpForm />
+      <div className={styles.signUpInputContainer}>
+        <TextInput
+          id="signup-text-input"
+          type="text"
+          label="이름"
+          placeholder="이름을 입력해주세요."
+        />
+        <CheckInput
+          label="이메일"
+          placeholder="이메일을 입력해주세요."
+          type="email"
+          id="email"
+        />
+        <CheckInput
+          label="닉네임"
+          placeholder="닉네임을 입력해주세요."
+          type="text"
+          id="nickname"
+        />
+        <PwInput
+          id="signup-pw-input"
+          label="비밀번호"
+          placeholder="비밀번호를 입력해주세요."
+        />
+        <PwInput
+          id="signup-pw-confirm-input"
+          label="비밀번호 확인"
+          placeholder="비밀번호를 입력해주세요."
+        />
+      </div>
+
+      <div className={styles.signUpBtnContainer}>
+        <Button label="회원가입" size="full" type="lined" />
       </div>
     </div>
   );
