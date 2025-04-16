9338adc352dddd3e2e99a6beb5d012723a592fe9
okang1014 <okang1014@gmail.com>
2025-04-16 09:41:04 +0900

    [design] app/components/textArea > props 및 디자인 일부 수정

--- app/components/textArea/TextArea.tsx
+++ app/components/textArea/TextArea.tsx
@@ -2,24 +2,29 @@ import React from "react";
 import styles from "./textArea.module.scss";
 
 interface TextAreaProps {
+  label?: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
   placeholder?: string;
 }
 
 const TextArea: React.FC<TextAreaProps> = ({
+  label,
   value,
   onChange,
   placeholder = "텍스트를 입력해주세요.",
 }) => {
   return (
-    <textarea
-      className={styles.textArea}
-      value={value}
-      onChange={onChange}
-      placeholder={placeholder}
-      rows={5}
-    />
+    <div>
+      <p className={styles["textarea-label"]}>{label}</p>
+      <textarea
+        className={styles.textArea}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        rows={5}
+      />
+    </div>
   );
 };
 
