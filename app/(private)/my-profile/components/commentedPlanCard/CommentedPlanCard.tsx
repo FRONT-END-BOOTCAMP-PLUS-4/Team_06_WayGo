806af517df6b0c1bff74bb1cf25bee2d51fe8e48
okang1014 <okang1014@gmail.com>
2025-04-16 15:10:27 +0900

    [feat] my-profile/components > 내 댓글이 달린 계획 카드 컴포넌트 생성

---
+++ app/(private)/my-profile/components/commentedPlanCard/CommentedPlanCard.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Button from "@/components/button/Button";
+import styles from "./commentedPlanCard.module.scss";
+import { useRouter } from "next/navigation";
+
+interface CommentedCardProps {
+  imageUrl: string;
+  comment: string;
+  title: string;
+}
+
+interface CommentedCardData {
+  data: CommentedCardProps;
+}
+
+const CommentedPlanCard = ({ data }: CommentedCardData) => {
+  console.log("카드", data);
+  const router = useRouter();
+
+  return (
+    <li className={styles["commented-card"]}>
+      <figure>
+        <Image src={data.imageUrl} alt="여행 계획 이미지" fill />
+      </figure>
+      <div className={styles["commented-card-content"]}>
+        <div>
+          <p className={styles["user-comment"]}>{data.comment}</p>
+          <p className={styles["plan-title"]}>{data.title}</p>
+        </div>
+        <Button
+          type="lined"
+          size="large"
+          label="계획보기"
+          onClick={() => {
+            router.replace("/plans/1");
+          }}
+        />
+      </div>
+    </li>
+  );
+};
+
+export default CommentedPlanCard;
