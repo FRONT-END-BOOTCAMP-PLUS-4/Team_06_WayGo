79707fe914eec2927959f214d7cbc66306a1b660
okang1014 <okang1014@gmail.com>
2025-04-16 15:10:06 +0900

    [feat] my-profile/components > 내 댓글이 달린 계획 레이아웃 컴포넌트화

---
+++ app/(private)/my-profile/components/commentedCardList/CommentedCardList.tsx
@@ -0,0 +1,24 @@
+import CommentedPlanCard from "@/(private)/my-profile/components/commentedPlanCard/CommentedPlanCard";
+import styles from "./commentedCardList.module.scss";
+
+interface CommentedCardProps {
+  id: number;
+  imageUrl: string;
+  comment: string;
+  title: string;
+}
+
+interface CommentedCardData {
+  data: CommentedCardProps[];
+}
+
+const CommentCardList = ({ data }: CommentedCardData) => {
+  const commentedCards = data.map((item) => (
+    <CommentedPlanCard data={item} key={item.id} />
+  ));
+
+  console.log("리스트", data);
+  return <ul className={styles["commented-plan-section"]}>{commentedCards}</ul>;
+};
+
+export default CommentCardList;
