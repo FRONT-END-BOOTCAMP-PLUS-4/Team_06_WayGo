4560de0eef54f69d6dff4fb22ee5f82363f82048
imtaejunk <imtaejunk@github.com>
2025-04-16 16:14:06 +0900

    use client 추가

--- app/(public)/plans/[id]/page.tsx
+++ app/(public)/plans/[id]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import Comments from "./components/comments/Comments";
 import TravelPlanOverview from "./components/travelPlanOverview/TravelPlanOverview";
