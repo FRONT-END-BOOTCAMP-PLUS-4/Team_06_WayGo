afa39de46566e8707fe8171f6b28f46993a520b3
okang1014 <okang1014@gmail.com>
2025-04-16 17:17:21 +0900

    [feat] 마이 프로필 페이지 추가 (#57)
    * [design] (private)/my-profile > 사용자 정보 카드 컴포넌트 생성
    
    * [feat] my-profile/components > 내 댓글이 달린 계획 레이아웃 컴포넌트화
    
    * [feat] my-profile/components > 내 댓글이 달린 계획 카드 컴포넌트 생성
    
    * [feat] my-profile > 마이 프로필 페이지 마크업 완료
    
    * [design] app > Image 태그 상위 요소 전체 position: relative 속성 부여
    
    * [chore] (private) > 아래의 모든 컴포넌트 use client 지정
    
    * [chore] my-profile > 컴포넌트 내 console.log() 제거

---
+++ app/(private)/my-profile/components/commentedCardList/CommentedCardList.tsx
@@ -0,0 +1,24 @@
+"use client";
+import CommentedPlanCard from "@/(private)/my-profile/components/commentedPlanCard/CommentedPlanCard";
+import styles from "./commentedCardList.module.scss";
+
+interface CommentedCardProps {
+  id: number;
+  imageUrl: string;
+  comment: string;
+  title: string;
+}
+
+interface CommentedCardData {
+  data: CommentedCardProps[];
+}
+
+const CommentCardList = ({ data }: CommentedCardData) => {
+  const commentedCards = data.map((item) => (
+    <CommentedPlanCard data={item} key={item.id} />
+  ));
+
+  return <ul className={styles["commented-plan-section"]}>{commentedCards}</ul>;
+};
+
+export default CommentCardList;
---
+++ app/(private)/my-profile/components/userInfoCard/UserInfoCard.tsx
@@ -0,0 +1,36 @@
+"use client";
+import Button from "@/components/button/Button";
+import Image from "next/image";
+import styles from "./userInfoCard.module.scss";
+
+interface UserInfoProps {
+  email: string;
+  nickname: string;
+  profileImg: string;
+}
+
+interface UserInfoCardProps {
+  userInfo: UserInfoProps;
+}
+
+const UserInfoCard = ({ userInfo }: UserInfoCardProps) => {
+  return (
+    <div className={styles["my-info-card"]}>
+      <figure>
+        <Image
+          src={userInfo.profileImg}
+          alt="사용자의 프로필 이미지"
+          width={100}
+          height={100}
+        />
+      </figure>
+      <div>
+        <p className={styles["user-nickname"]}>{userInfo.nickname}</p>
+        <p className={styles["user-email"]}>{userInfo.email}</p>
+      </div>
+      <Button size="large" type="lined" label="내 정보 수정" />
+    </div>
+  );
+};
+
+export default UserInfoCard;
