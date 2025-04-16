fa34d544438ae5973da7de454913a1ae4d587161
jeongtunjang <jjy306105@gmail.com>
2025-04-16 15:11:13 +0900

    feat: not-found 페이지 > UI 반영

---
+++ app/not-found.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import styles from "./not-found.module.scss";
+
+export default function NotFound() {
+  return (
+    <div className={styles["not-found"]}>
+      <div className={styles["error-container"]}>
+        <div className={styles["error-text"]}>
+          {"요청하신 페이지를 찾지 못했어요 😭"}
+        </div>
+        <Image
+          src={"/logos/char-error.svg"}
+          alt="no result image"
+          width={224}
+          height={156}
+        />
+      </div>
+    </div>
+  );
+}
