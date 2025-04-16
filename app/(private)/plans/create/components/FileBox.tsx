13cc511ab8050746faec6fd54be86c78d21697ad
okang1014 <okang1014@gmail.com>
2025-04-16 09:42:13 +0900

    [feat] create/components/fileBox > 파일 박스 마크업

---
+++ app/(private)/plans/create/components/FileBox.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import styles from "./fileBox.module.scss";
+
+interface FileBoxProps {
+  label?: string;
+  multiple?: boolean;
+}
+
+const FileBox = ({ label, multiple = true }: FileBoxProps) => {
+  return (
+    <section className={styles["filebox-container"]}>
+      <p>{label}</p>
+      <div className={styles.filebox}>
+        <div>
+          <Image
+            src="/icons/upload-icon.svg"
+            width={40}
+            height={40}
+            alt="이미지 파일 업로드"
+          />
+          <p className={styles["filebox-text"]}>
+            이미지를 드래그하거나 클릭하여 업로드
+          </p>
+          <p className={styles["filebox-caption"]}>
+            PNG, JPG, JPEG (최대 10MB)
+          </p>
+        </div>
+        <label htmlFor="main-image">이미지 선택</label>
+        <input
+          id="main-image"
+          type="file"
+          accept=".png, .jpg, .jpeg"
+          multiple={multiple}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default FileBox;
