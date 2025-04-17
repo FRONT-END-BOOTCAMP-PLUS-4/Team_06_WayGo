4447f4eccf44e3fcf5c9782bf9fead63be7702cc
okang1014 <okang1014@gmail.com>
2025-04-17 13:49:25 +0900

    [chore] application/uscase/plans > CreatePlanDto.ts, EditPlanDto.ts 설정

---
+++ application/usecases/plans/dto/CreatePlanDto.ts
@@ -0,0 +1,10 @@
+export interface CreateTravelPlanDto {
+  title: string;
+  schedule: string;
+  details: string;
+  travelTips: string;
+  durationId: number;
+  locationId: number;
+  budgetId: number;
+  seasonId: number;
+}
