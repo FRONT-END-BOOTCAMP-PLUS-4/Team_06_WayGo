afb0ed4347b86afa779716d8dad105f43fe65ded
jeongtunjang <jjy306105@gmail.com>
2025-04-18 10:51:37 +0900

    feat: PlanListDto 수정, PlanListCurationDto 추가 > 큐레이션과 검색 결과 Dto 분리

--- application/usecases/plans/dto/PlanListCuratitonDto.ts
+++ application/usecases/plans/dto/PlanListCuratitonDto.ts
@@ -1,8 +1,7 @@
 import { PlanCardDto } from "./PlanCardDto";
 
 export interface PlanListDto {
+  title?: string | null;
   plans: PlanCardDto[];
   totalCount?: number | null;
-  totalPages?: number | null;
-  currentPage?: number | null;
 }
