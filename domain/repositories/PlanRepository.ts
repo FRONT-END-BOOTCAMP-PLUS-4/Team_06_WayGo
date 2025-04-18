bc7f7a55e60aae3efeefde0f154ef2dae7eb1d0c
jeongtunjang <jjy306105@gmail.com>
2025-04-18 14:50:19 +0900

    fix: PlanRepository > findAll 매개변수 추가, 큐레이션 조회 추가

--- domain/repositories/PlanRepository.ts
+++ domain/repositories/PlanRepository.ts
@@ -1,8 +1,12 @@
 import { Plan } from "domain/entities/Plan";
+import { PlanFilterDto } from "application/usecases/plans/dto/PlanFilterDto";
 
 export interface PlanRepository {
-  findAll(): Promise<Plan[]>;
+  findAll(filter: PlanFilterDto): Promise<Plan[]>; // 필터 기반 목록 조회
   findById(id: number): Promise<Plan | null>;
   save(plan: Plan): Promise<Plan>;
   delete(id: number): Promise<void>;
+
+  findPopularPlans(): Promise<Plan[]>; // 댓글 많은 순 인기 계획
+  findCurrentSeasonPlans(seasonId: number): Promise<Plan[]>; // 계절별 큐레이션
 }
