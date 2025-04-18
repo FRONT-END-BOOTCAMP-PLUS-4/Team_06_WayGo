0a14bd9ba8f607dce179b2e8077ae0db62d6ede7
imtaejunk <imtaejunk@github.com>
2025-04-18 11:26:25 +0900

    드롭다운 ui

--- application/usecases/comments/DeleteCommentUsecase.ts
+++ application/usecases/comments/DeleteCommentUsecase.ts
@@ -1,9 +1,19 @@
 import { CommentRepository } from "domain/repositories/CommentRepository";
+import { DeleteCommentDto } from "./dto/DeleteCommentDto";
 
 export class DeleteCommentUsecase {
   constructor(private commentRepository: CommentRepository) {}
 
-  async execute(commentId: number): Promise<void> {
-    await this.commentRepository.delete(commentId);
+  async execute(dto: DeleteCommentDto): Promise<void> {
+    const comment = await this.commentRepository.findById(dto.id);
+    if (!comment) {
+      throw new Error("댓글을 찾을 수 없습니다");
+    }
+
+    if (comment.userId !== dto.userId) {
+      throw new Error("삭제 권한이 없습니다");
+    }
+
+    await this.commentRepository.delete(dto.id);
   }
 }
