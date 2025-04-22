c0c02b9b209be8f5e2d0085be2dab2ce40ece430
jeongtunjang <jjy306105@gmail.com>
2025-04-22 11:12:01 +0900

    feat: plan 검색 결과 페이지 > 카테고리 데이터 fetching, 더미 데이터 제거

--- app/(public)/plans/page.tsx
+++ app/(public)/plans/page.tsx
@@ -7,40 +7,41 @@ import SearchInput from "@/components/searchInput/SearchInput";
 import styles from "./plans.module.scss";
 import Button from "@/components/button/Button";
 import Image from "next/image";
+import { useState, useEffect } from "react";
+import { CategoryResponse } from "application/usecases/plans/dto/CategoryListDto";
 
 const PlansPage = () => {
   const keyword = "검색 키워드";
   const resultCnt = 16;
 
-  const durationOptionList = [
-    { value: 1, title: "당일치기" },
-    { value: 2, title: "1박2일" },
-    { value: 3, title: "2박3일" },
-    { value: 4, title: "3박4일~" },
-  ];
+  const [categoryOptions, setCategoryOptions] = useState<CategoryResponse>({
+    season: [],
+    duration: [],
+    budget: [],
+    location: [],
+  });
 
-  const seasonOptionList = [
-    { value: 1, title: "봄 🌸" },
-    { value: 2, title: "여름 🤿" },
-    { value: 3, title: "가을 🍁" },
-    { value: 4, title: "겨울 ❄️" },
-  ];
+  useEffect(() => {
+    const fetchCategoryOptions = async () => {
+      try {
+        const res = await fetch("/api/category");
+        if (!res.ok) {
+          throw new Error("카테고리 데이터를 불러오지 못했습니다.");
+        }
 
-  const locationOptionList = [
-    { value: 1, title: "수도권" },
-    { value: 2, title: "강원권" },
-    { value: 3, title: "충청권" },
-    { value: 4, title: "호남권" },
-    { value: 5, title: "경상권" },
-    { value: 6, title: "제주권" },
-  ];
+        const data: CategoryResponse = await res.json();
+        setCategoryOptions(data);
+      } catch (err) {
+        console.error("카테고리 데이터 fetch 실패:", err);
+      }
+    };
 
-  const budgetOptionList = [
-    { value: 1, title: "~10만원" },
-    { value: 2, title: "10~20만원" },
-    { value: 3, title: "20~40만원" },
-    { value: 4, title: "40만원~" },
-  ];
+    fetchCategoryOptions();
+  }, []);
+
+  useEffect(() => {
+    console.log("categoryOptions: ", categoryOptions);
+  }, [categoryOptions]);
 
   return (
     <div className="main-container">
@@ -58,10 +59,34 @@ const PlansPage = () => {
         <>
           <div className={styles["category-container"]}>
             <div className={styles["category-wrapper"]}>
-              <SelectBasic option={durationOptionList} placeholder={"기간"} />
-              <SelectBasic option={seasonOptionList} placeholder={"예산"} />
-              <SelectBasic option={locationOptionList} placeholder={"지역"} />
-              <SelectBasic option={budgetOptionList} placeholder={"계절"} />
+              <SelectBasic
+                option={categoryOptions.duration.map((item) => ({
+                  value: item.id,
+                  title: item.content,
+                }))}
+                placeholder={"기간"}
+              />
+              <SelectBasic
+                option={categoryOptions.budget.map((item) => ({
+                  value: item.id,
+                  title: item.content,
+                }))}
+                placeholder={"예산"}
+              />
+              <SelectBasic
+                option={categoryOptions.location.map((item) => ({
+                  value: item.id,
+                  title: item.content,
+                }))}
+                placeholder={"지역"}
+              />
+              <SelectBasic
+                option={categoryOptions.season.map((item) => ({
+                  value: item.id,
+                  title: item.content,
+                }))}
+                placeholder={"계절"}
+              />
             </div>
             <Button size={"large"} label={"필터 적용"} type={"default"} />
           </div>
