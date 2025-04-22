6e9a4b34ce53dfb5bf222d283aaa59f47a41831f
okang1014 <okang1014@gmail.com>
2025-04-22 16:36:55 +0900

    [feat] member/plans/complete > 여행 계획 등록 완료 페이지에서 버튼 선택시 라우팅 추가

--- app/member/plans/complete/page.tsx
+++ app/member/plans/complete/page.tsx
@@ -3,7 +3,13 @@
 import Image from "next/image";
 import styles from "./complete.module.scss";
 import Button from "@/components/button/Button";
+import { useRouter, useSearchParams } from "next/navigation";
+
 const CompletePlan: React.FC = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
+
   return (
     <div className={`${styles["complete-container"]} main-container`}>
       <div className={styles["complete-text"]}>
@@ -16,8 +22,22 @@ const CompletePlan: React.FC = () => {
         height={156}
       />
       <div className={styles["action-container"]}>
-        <Button size="large" type="lined" label="메인으로" />
-        <Button size="large" type="default" label="내 계획으로" />
+        <Button
+          size="large"
+          type="lined"
+          label="메인으로"
+          onClick={() => {
+            router.push("/");
+          }}
+        />
+        <Button
+          size="large"
+          type="default"
+          label="내 계획으로"
+          onClick={() => {
+            router.push(`/member/plans/${id}`);
+          }}
+        />
       </div>
     </div>
   );
