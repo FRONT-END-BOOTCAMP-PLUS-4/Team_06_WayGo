2f18330b9a8425e4b6a8392e515ebf572c3029d2
okang1014 <okang1014@gmail.com>
2025-04-22 09:44:07 +0900

    [feat] member/plans/create > FileBox 미리보기 기능 구현 중

--- app/member/plans/create/components/fileBox/FileBox.tsx
+++ app/member/plans/create/components/fileBox/FileBox.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import styles from "./fileBox.module.scss";
 import { useDropzone } from "react-dropzone";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface FileBoxProps {
   label?: string;
@@ -21,6 +21,12 @@ const FileBox = ({
 }: FileBoxProps) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
+  interface FileWithPreview extends File {
+    preview: string;
+  }
+
+  const [files, setFiles] = useState<FileWithPreview[]>([]);
+
   const { getRootProps, getInputProps, open, acceptedFiles } = useDropzone({
     multiple: multiple,
     maxFiles: maxFiles,
@@ -28,54 +34,71 @@ const FileBox = ({
       "image/jpeg": [".jpeg", ".jpg"],
       "image/png": [".png"],
     },
-    onDrop: (incomingFiles) => {
-      if (inputRef.current) {
-        const dataTransfer = new DataTransfer();
-        incomingFiles.forEach((v) => {
-          dataTransfer.items.add(v);
-        });
-        inputRef.current.files = dataTransfer.files;
-      }
+    onDrop: (acceptedFiles) => {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
     },
   });
 
-  const files = acceptedFiles.map((file) => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-    </li>
+  const thumbs = files.map((file) => (
+    <div className={styles.thumb} key={file.name}>
+      <div className={styles["thumb-inner"]}>
+        <Image
+          alt={file.name}
+          src={file.preview}
+          // Revoke data uri after image is loaded
+          onLoad={() => {
+            URL.revokeObjectURL(file.preview);
+          }}
+          fill
+        />
+      </div>
+    </div>
   ));
+
+  useEffect(() => {
+    // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
+    return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
+  }, [files]);
 
   return (
     <section className={styles["filebox-container"]}>
       <p>{label}</p>
-      <div className={`${styles.filebox} dropzone`} {...getRootProps()}>
-        <div>
-          <Image
-            src="/icons/upload-icon.svg"
-            width={40}
-            height={40}
-            alt="이미지 파일 업로드"
+      {files?.length === 0 ? (
+        <div className={`${styles.filebox} dropzone`} {...getRootProps()}>
+          <div>
+            <Image
+              src="/icons/upload-icon.svg"
+              width={40}
+              height={40}
+              alt="이미지 파일 업로드"
+            />
+            <p className={styles["filebox-text"]}>
+              이미지를 드래그하거나 클릭하여 업로드
+            </p>
+            <p className={styles["filebox-caption"]}>
+              PNG, JPG, JPEG (최대 10MB)
+            </p>
+          </div>
+          <label htmlFor="main-image">이미지 선택</label>
+          <input
+            ref={inputRef}
+            {...getInputProps({
+              name: name,
+              required: required,
+            })}
           />
-          <p className={styles["filebox-text"]}>
-            이미지를 드래그하거나 클릭하여 업로드
-          </p>
-          <p className={styles["filebox-caption"]}>
-            PNG, JPG, JPEG (최대 10MB)
-          </p>
         </div>
-        <label htmlFor="main-image">이미지 선택</label>
-        <input
-          ref={inputRef}
-          {...getInputProps({
-            name: name,
-            required: required,
-          })}
-        />
-      </div>
-      <aside>
-        <h4>Files</h4>
-        <ul>{files}</ul>
-      </aside>
+      ) : (
+        <aside>
+          <ul className={styles["thumbs-container"]}>{thumbs}</ul>
+        </aside>
+      )}
     </section>
   );
 };
