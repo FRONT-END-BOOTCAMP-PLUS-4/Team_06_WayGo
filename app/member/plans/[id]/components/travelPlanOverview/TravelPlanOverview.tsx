c0b310d41b88348324b9cd2b77eda78017045abd
okang1014 <okang1014@gmail.com>
2025-04-23 16:01:40 +0900

    [feat] app/member/plans/[id] > 여행 계획 상세 페이지 member 폴더에 추가

--- app/member/plans/[id]/components/travelPlanOverview/TravelPlanOverview.tsx
+++ app/member/plans/[id]/components/travelPlanOverview/TravelPlanOverview.tsx
@@ -5,20 +5,34 @@ import React from "react";
 import styles from "./travelPlanOverview.module.scss";
 import OverviewCarousel from "./overviewCarousel/OverviewCarousel";
 
-const TravelPlanOverview: React.FC = () => {
+interface OverviewDataProps {
+  user: {
+    nickname: string;
+  };
+  createdAt: string;
+  title: string;
+  images: string[];
+  budget: string;
+  duration: string;
+  location: string;
+  season: string;
+}
+
+interface DataProps {
+  data: OverviewDataProps;
+}
+
+const TravelPlanOverview = ({ data }: DataProps) => {
+  const imageList = data?.images.map((image) => {
+    return image.imgUrl;
+  });
+
+  console.log("개요 데이터: ", data);
   return (
     <div className={styles.overviewContainer}>
       <div className={styles.leftColumn}>
         <div className={styles.carouselContainer}>
-          <OverviewCarousel
-            images={[
-              "/images/jeju.jpg",
-              "/images/yeosu.jpg",
-              "/images/jeju.jpg",
-              "/images/yeosu.jpg",
-              "/images/jeju.jpg",
-            ]}
-          />
+          <OverviewCarousel images={imageList} />
         </div>
       </div>
 
@@ -27,30 +41,30 @@ const TravelPlanOverview: React.FC = () => {
         <div className={styles.topContent}>
           <div className={styles.infoBoxes}>
             <div className={styles.boxHeader}>
-              <div className={styles.author}>작성자명</div>
-              <div className={styles.date}>2025.04.10</div>
+              <div className={styles.author}>{data?.user.nickname}</div>
+              <div className={styles.date}>{data?.createdAt}</div>
             </div>
 
-            <div className={styles.boxTitle}>제주도 가보자고.</div>
+            <div className={styles.boxTitle}>{data?.title}</div>
 
             <div className={styles.infoRow}>
               <div className={styles.infoLabel}>지역</div>
-              <div className={styles.infoValue}>제주권</div>
+              <div className={styles.infoValue}>{data?.location}</div>
             </div>
 
             <div className={styles.infoRow}>
               <div className={styles.infoLabel}>기간</div>
-              <div className={styles.infoValue}>2박 3일</div>
+              <div className={styles.infoValue}>{data?.duration}</div>
             </div>
 
             <div className={styles.infoRow}>
               <div className={styles.infoLabel}>계절</div>
-              <div className={styles.infoValue}>여름</div>
+              <div className={styles.infoValue}>{data?.season}</div>
             </div>
 
             <div className={styles.infoRow}>
               <div className={styles.infoLabel}>예산</div>
-              <div className={styles.infoValue}>20~40만원</div>
+              <div className={styles.infoValue}>{data?.budget}</div>
             </div>
           </div>
         </div>
--- app/member/plans/[id]/page.tsx
+++ app/member/plans/[id]/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Comments from "./components/comments/Comments";
 import TravelPlanOverview from "./components/travelPlanOverview/TravelPlanOverview";
 import TripGuide from "./components/tripGuide/TripGuide";
 import styles from "./detail.module.scss";
+import DOMPurify from "dompurify";
+import { useParams } from "next/navigation";
+import { ClipLoader } from "react-spinners";
 
 interface DetailPageProps {
   params: {
@@ -12,19 +15,97 @@ interface DetailPageProps {
   };
 }
 
-const DetailPage: React.FC<DetailPageProps> = ({ params }) => {
-  const planId = Number(params.id);
+const DetailPage: React.FC<DetailPageProps> = () => {
+  const planId = Number(useParams().id);
+  const [isLoading, setIsLoading] = useState(true);
+
+  interface OverviewDataProps {
+    user: {
+      id: string;
+      nickname: string;
+      profileImage?: string | null;
+    };
+    createdAt: string;
+    title: string;
+    images: string[];
+    budget: string;
+    duration: string;
+    location: string;
+    season: string;
+  }
+
+  interface GuideDataProps {
+    schedule: string;
+    details: string;
+    travelTips: string;
+  }
+
+  const [overviewData, setOverviewData] = useState<OverviewDataProps | null>(
+    null
+  );
+  const [guideData, setGuideData] = useState<GuideDataProps | null>(null);
+
+  const fetchPlanDetail = async (id: number) => {
+    try {
+      const response = await fetch(`/api/plans/${id}`);
+
+      if (!response.ok) {
+        throw new Error("여행 계획 조회 실패");
+      }
+
+      const planDataResponse = await response.json();
+      const planData = await planDataResponse.data;
+
+      setOverviewData({
+        user: planData?.user,
+        createdAt: planData?.createdAt.split("T")[0],
+        title: planData?.title,
+        images: planData?.images,
+        budget: planData?.budget.content,
+        duration: planData?.duration.content,
+        location: planData?.location.content,
+        season: planData?.season.content,
+      });
+
+      setGuideData({
+        schedule: DOMPurify.sanitize(planData?.schedule),
+        details: DOMPurify.sanitize(planData?.details),
+        travelTips: DOMPurify.sanitize(planData?.travelTips),
+      });
+
+      setIsLoading(false);
+    } catch (error) {
+      console.error("여행 계획 조회 실패: ", error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    console.log("여행 계획 데이터", fetchPlanDetail(planId));
+  }, [planId]);
 
   return (
-    <div className={styles.pageContainer}>
-      <div className={styles.overviewContainer}>
-        <TravelPlanOverview />
-      </div>
-      <div className={styles.guideContainer}>
-        <TripGuide />
-      </div>
-      <div className={styles.commentsContainer}>
-        <Comments planId={planId} />
+    <div className="main-container">
+      <div className={styles.pageContainer}>
+        {isLoading ? (
+          <div className={styles.overviewContainer}>
+            <div style={{ textAlign: "center" }}>
+              <ClipLoader color="#216c99" loading size={80} />
+            </div>
+          </div>
+        ) : (
+          <>
+            <div className={styles.overviewContainer}>
+              <TravelPlanOverview data={overviewData} />
+            </div>
+            <div className={styles.guideContainer}>
+              <TripGuide data={guideData || undefined} />
+            </div>
+            <div className={styles.commentsContainer}>
+              <Comments planId={planId} />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
