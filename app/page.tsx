0fa0ef3a697c03cbd4afdba16026ab3acca7ad74
okang1014 <okang1014@gmail.com>
2025-04-24 23:32:44 +0900

    [feat] app > page.tsx 계절별 큐레이션 추가

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,55 @@ import Button from "@/components/button/Button";
 import SearchInput from "@/components/searchInput/SearchInput";
 import styles from "./page.module.scss";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import LoadingArea from "@/components/loadingArea/LoadingArea";
+
+interface Plan {
+  created_at: string;
+}
 
 export default function Home() {
   const router = useRouter();
 
-  const seasonId = 1;
+  const [seasonList, setSeasonList] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const month = new Date().getMonth() + 1;
+  let currentSeasonId: number;
+  let currentSeasonName: string;
+
+  if ([3, 4, 5].includes(month)) {
+    currentSeasonId = 1;
+    currentSeasonName = "🌸 봄";
+  } else if ([6, 7, 8].includes(month)) {
+    currentSeasonId = 2;
+    currentSeasonName = "🌊 여름";
+  } else if ([9, 10, 11].includes(month)) {
+    currentSeasonId = 3;
+    currentSeasonName = "🍁 가을";
+  } else {
+    currentSeasonId = 4;
+    currentSeasonName = "❄️ 겨울";
+  }
 
   const fetchSeasonPlanList = async () => {
+    setIsLoading(true);
     try {
-      const response = await fetch("/api/plans/season?seasonId=2");
+      const response = await fetch(
+        `/api/plans/season?seasonId=${currentSeasonId}`
+      );
 
       const result = await response.json();
-      console.log(result);
+      const sortedSeasonList = result.data.plans.sort((a: Plan, b: Plan) => {
+        return (
+          new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+        );
+      });
+
+      console.log(sortedSeasonList);
+      setSeasonList(sortedSeasonList);
+      setIsLoading(false);
     } catch (error) {
       console.error("계절별 여행 목록 조회 실패", error);
     }
@@ -49,8 +85,20 @@ export default function Home() {
       <div className={styles["search-container"]}>
         <SearchInput />
       </div>
-      <PlanCardList />
-      <PlanCardList />
+      {isLoading ? (
+        <div className={styles["loading-area"]}>
+          <LoadingArea />
+        </div>
+      ) : (
+        <>
+          <PlanCardList
+            titleName={`${currentSeasonName}에 여행가요 🚌`}
+            plans={seasonList}
+            isScrollAvailable={true}
+          />
+          <PlanCardList />
+        </>
+      )}
     </div>
   );
 }
