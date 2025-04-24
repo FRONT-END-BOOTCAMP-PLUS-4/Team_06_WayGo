00f95f0fe9280f3ead907aad337c12d0fade730e
okang1014 <okang1014@gmail.com>
2025-04-24 09:51:28 +0900

    [feat] (public), member > layout 수정하여 회원 비회원 라우팅 수정

--- app/member/layout.tsx
+++ app/member/layout.tsx
@@ -9,20 +9,15 @@ interface LayoutProps {
 }
 
 function MemberLayout({ children }: LayoutProps) {
-  const { id, isRehydrated } = useAuthStore();
+  const { id } = useAuthStore();
   const router = useRouter();
 
   useEffect(() => {
     // 로그인되지 않은 경우 member 로 시작하는 페이지에 접속했을 때 login 페이지로 이동
-    if (id === null && isRehydrated) {
+    if (!id) {
       router.replace("/login");
     }
-  }, [id, isRehydrated, router]);
-
-  // 상태가 초기화되지 않았거나 인증되지 않은 경우 아무것도 렌더링하지 않음
-  if (!isRehydrated || !id) {
-    return null;
-  }
+  }, [id, router]);
 
   // 인증된 사용자의 경우 children 렌더링
   return <>{children}</>;
